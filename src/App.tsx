5a07eb1ea31196ea0efa18705c9534ccc8249dc8
Coolbreeze <agbokaka8@gmail.com>
2025-02-19 05:07:03 -0800

    set up commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import About from "./components/About/About";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <main>
-      <div>
-        <Navbar />
-      </div>
-      <div>
+      <Navbar>
         <Home />
-      </div>
+      </Navbar>
     </main>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import {Search} from "@mui/icons-material";
 
-const Navbar = () => {
+interface NavProps {
+  children: React.ReactElement;
+}
+
+const Navbar = ({ children }: NavProps) => {
+  const [searchItems, setSearchItems] = useState<string | number>("");
+
   return (
-    <main>
-      <section className="navbarSection">
-        <div className="navbarDiv1">
-          Summer Sale For All suits And Free Express Delivery - OFF 50%!
-          <a href="">ShopNow</a>
-        </div>
-        <div className="navbarDiv2"></div>
-      </section>
+    <main className="nav-container">
+      <nav className="nav-wrapper">
+        <section className="nav-description">
+          <div>
+            Summer Sale For All suits And Free Express Delivery - OFF 50%!
+            <a href="" className="nav-shop-link">
+              ShopNow
+            </a>
+          </div>
+          <div className="nav-language">English</div>
+        </section>
+        <section className="nav-navigation">
+          <h2>Exclusive</h2>
+          <div>
+            <a href="">Home</a>
+          </div>
+          <div>
+            <a href="">Contact</a>
+          </div>
+          <div>
+            <a href="">About</a>
+          </div>
+          <div>
+            <a href="">Sign Up</a>
+          </div>
+          <div className="nav-search">
+            <input
+              className="nav-search-field"
+              type="text"
+              name="search"
+              placeholder="What are you looking for?"
+              value={searchItems}
+              onChange={(e) => setSearchItems(e.target.value)}
+            />
+            <Search className="nav-search-icon" />
+          </div>
+        </section>
+      </nav>
+      <div className="nav-children">{children}</div>
     </main>
   );
 };
