135d50c6e6ca5e1bf26951d5b1aec0a50970d8f2
Coolbreeze <agbokaka8@gmail.com>
2025-04-09 00:02:20 +0100

    auth-commit

--- src/components/Authentication/Login/Login.tsx
+++ src/components/Authentication/Login/Login.tsx
@@ -4,12 +4,15 @@ import CustomInput from "../../CustomInput/CustomInput";
 import CustomButton from "../../CustomButton/CustomButton";
 import authimg from "../../../assets/authimg.png";
 import { useNavigate } from "react-router-dom";
+import { signIn } from "../../../controller/userController";
+import { useDispatch } from "react-redux";
+import Navbar from "../../Navbar/Navbar";
 
 const Login = () => {
   interface formType {
-    email?: string;
-    phoneNumber?: string;
-    password?: string;
+    email: string;
+    phoneNumber: string;
+    password: string;
   }
 
   const initialState: formType = {
@@ -20,9 +23,11 @@ const Login = () => {
 
   const [formData, setFormData] = useState<formType>(initialState);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    signIn(formData, dispatch);
 
     setFormData(initialState);
   };
@@ -33,54 +38,59 @@ const Login = () => {
   };
 
   return (
-    <main className="login-container">
-      <section className="Login-image-sec">
-        <div>
-          <img src={authimg} alt="phone image" className="login-image" />
-        </div>
-      </section>
-      <section className="login-form-sec">
-        <form onSubmit={(event) => handleSubmit(event)} className="login-form">
-          <header>Log in to Exclusive</header>
-          <div className="login-form-details">Enter your details below</div>
+    <Navbar>
+      <main className="login-container">
+        <section className="Login-image-sec">
+          <div>
+            <img src={authimg} alt="phone image" className="login-image" />
+          </div>
+        </section>
+        <section className="login-form-sec">
+          <form
+            onSubmit={(event) => handleSubmit(event)}
+            className="login-form"
+          >
+            <header>Log in to Exclusive</header>
+            <div className="login-form-details">Enter your details below</div>
 
-          <div>
-            <CustomInput
-              type="email"
-              name="email"
-              placeholder="Email or Phone Number"
-              className="login-input"
-              value={formData.email}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <CustomInput
-              type="password"
-              name="password"
-              placeholder="Password"
-              className="login-input"
-              value={formData.password}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <div className="login-btn-wrapper">
-              <CustomButton
-                type="submit"
-                text="Log In"
-                className="login-btn"
+            <div>
+              <CustomInput
+                type="email"
+                name="email"
+                placeholder="Email or Phone Number"
+                className="login-input"
+                value={formData.email}
+                onChange={handleChange}
               />
-              <div>Forget Password?</div>
             </div>
-            <p className="login-form-signup-toggle">
-              Dont have an account?{" "}
-              <span onClick={() => navigate("/register")}>Sign Up</span>
-            </p>
-          </div>
-        </form>
-      </section>
-    </main>
+            <div>
+              <CustomInput
+                type="password"
+                name="password"
+                placeholder="Password"
+                className="login-input"
+                value={formData.password}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <div className="login-btn-wrapper">
+                <CustomButton
+                  type="submit"
+                  text="Log In"
+                  className="login-btn"
+                />
+                <div>Forget Password?</div>
+              </div>
+              <p className="login-form-signup-toggle">
+                Dont have an account?{" "}
+                <span onClick={() => navigate("/register")}>Sign Up</span>
+              </p>
+            </div>
+          </form>
+        </section>
+      </main>
+    </Navbar>
   );
 };
 
