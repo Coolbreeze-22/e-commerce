763aa625dda9ca327f9be6c4ab1f0f69cf9602e3
Coolbreeze <agbokaka8@gmail.com>
2025-05-07 23:24:18 +0100

    final-setup

--- src/dummy/dummyCountdown.ts
+++ src/dummy/dummyCountdown.ts
@@ -1,6 +1,6 @@
 import {CountdownType} from '../states/redux/reducerTypes'
 
 export const flashSaleCountdown: CountdownType = {
-    startDate: "2025-04-29T14:00:00",
-    endDate: "2025-04-29T16:00:00",
+    startDate: "2025-05-03T00:00:00",
+    endDate: "2025-05-05T00:00:00",
   };
