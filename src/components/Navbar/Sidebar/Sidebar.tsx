eb953d5518d47e50adb43a12ca14a03a4e54ec1c
Coolbreeze <agbokaka8@gmail.com>
2025-05-12 21:42:10 +0100

    add-wishlist-to-sidebar

--- src/components/Navbar/Sidebar/Sidebar.tsx
+++ src/components/Navbar/Sidebar/Sidebar.tsx
@@ -14,6 +14,8 @@ import { UserProps } from "../../../states/redux/reducerTypes";
 import * as utils from "../navUtils";
 import { ClickAwayListener } from "@mui/material";
 import { useStateContext } from "../../../context/context";
+import { FiShoppingCart } from "react-icons/fi";
+import { FaRegHeart } from "react-icons/fa6";
 
 type sidebarPropType = {
   user: UserProps;
@@ -22,7 +24,7 @@ type sidebarPropType = {
 
 const Sidebar = ({ user, handleLogout }: sidebarPropType) => {
   const useLabelNavigate = utils.useLabelNavigate();
-  const {  setIsSidebar } = useStateContext();
+  const { setIsSidebar } = useStateContext();
 
   const handleNavigate = (label: string) => {
     useLabelNavigate(label);
@@ -90,6 +92,26 @@ const Sidebar = ({ user, handleLogout }: sidebarPropType) => {
           </span>
           <span>About</span>
         </div>
+        <div
+          onClick={() => {
+            handleNavigate("cart");
+          }}
+        >
+          <span>
+            <FiShoppingCart size={20} />
+          </span>
+          <span>Cart</span>
+        </div>
+        <div
+          onClick={() => {
+            handleNavigate("wishlist");
+          }}
+        >
+          <span>
+            <FaRegHeart size={20} />
+          </span>
+          <span>Wishlist</span>
+        </div>
         {user.id ? (
           <>
             <div
@@ -121,16 +143,6 @@ const Sidebar = ({ user, handleLogout }: sidebarPropType) => {
                 <ImCancelCircle size={20} />
               </span>
               <span>My Cancellations</span>
-            </div>
-            <div
-              onClick={() => {
-                handleNavigate("account");
-              }}
-            >
-              <span>
-                <FaRegStar size={20} />
-              </span>
-              <span>My Reviews</span>
             </div>
             <div
               onClick={() => {
