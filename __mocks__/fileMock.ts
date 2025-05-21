5f70465d6fe6c1b99eb08e7d430b8391d7cd3a3b
Coolbreeze <agbokaka8@gmail.com>
2025-05-21 21:08:12 +0100

    gitignore2

--- __mocks__/fileMock.ts
+++
@@ -1,3 +0,0 @@
-export default 'test-file-stub';
-
-// no need to install jest-transform-stub
--- __mocks__/react-router-dom.ts
+++
@@ -1,5 +0,0 @@
-export const useNavigate = jest.fn();
-export const useParams = jest.fn();
-export const useLocation = jest.fn();
-export const Link = jest.fn().mockImplementation(({ children }) => children);
-// export const Outlet = jest.fn().mockImplementation(({ children }) => children);
