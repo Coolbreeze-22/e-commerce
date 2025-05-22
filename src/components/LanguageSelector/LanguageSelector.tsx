8d3a4c88fd6c4c74d2aaeef37aa047655e14a3a5
Coolbreeze <agbokaka8@gmail.com>
2025-05-22 14:10:01 +0100

    local-bug

--- src/components/LanguageSelector/LanguageSelector.tsx
+++ src/components/LanguageSelector/LanguageSelector.tsx
@@ -1,19 +1,7 @@
-// import GoogleTranslateSelect from "@google-translate-select/react";
 import "./LanguageSelector.css";
 
 const LanguageSelector = () => {
-  return (
-    <div className="lang-wrapper">
-      {/* <GoogleTranslateSelect
-        defaultPageLanguageCode="en"
-        // includedLanguages={['en', 'fr', 'de', 'es', 'zh-CN']}
-        // layout="SIMPLE"
-        className="lang"
-      /> */}
-      coOLBREeze
-      
-    </div>
-  );
+  return <div className="lang-wrapper">coOLBREeze</div>;
 };
 
 export default LanguageSelector;
